3b57ebfb9ec1e1a290d0ffa3afc7438b6d077960
dev-Jxmes <ariclenealfredorgmail.com>
2026-01-21 23:36:22 +0100

    First Configurations

---
+++ rest-with-spring-boot-and-java/src/test/java/ao/com/dev/StartupTests.java
@@ -0,0 +1,13 @@
+package ao.com.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
